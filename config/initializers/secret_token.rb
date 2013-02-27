3648b7bb126c13c8faa3d6c1a63f826da48c1810
Christopher Kolstad <christopher.kolstad@gmail.com>
2013-02-27 22:19:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isharelib::Application.config.secret_token = 'cfa281e205cac98527f551e9184cfd4232100ed0b2cd3dbdfc9c5e5dfb0a816ff4476cfe2511dbc2f5275ef0afc04494a06b3d110097cb65ef16cecb51f05fce'
